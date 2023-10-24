a71153a731f06b8f9cb95cb1bcd86373394b31e1
hslim <myohoon95@gmail.com>
2023-10-25 02:26:05 +0900

    modify activity. appcompat -> compose

--- util/build.gradle.kts
+++ util/build.gradle.kts
@@ -38,7 +38,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.activity:activity-compose:1.8.0")
 
     testImplementation("junit:junit:4.13.2")
 
